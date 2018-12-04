991907d111574d63cfa6b3cbac55cf3f6131a7bd
kaichun <w2508118341@gmail.com>
2018-12-03 19:20:51 -0500

    cancel warning

--- final/info6205_FinalProject/src/test/ga/gene/PointTest.java
+++ final/info6205_FinalProject/src/test/ga/gene/PointTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
-@SuppressWarnings("All")
 
 class PointTest {
 	
